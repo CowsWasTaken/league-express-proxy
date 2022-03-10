9a7dd7c7c8afca6d39e3d66764f644a1911f201a
Felix J <felix.hannes.fj@gmail.com>
2022-03-10 21:15:48 +0100

    removed build folder

--- build/config/QUERY_PARAMS.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.QUERY_PARAMS = void 0;
-exports.QUERY_PARAMS = {
-    MAX_COUNT: 100,
-};
--- build/config/api_config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ApiConfig = void 0;
-exports.ApiConfig = {
-    timeoutTime: 120 // in seconds
-};
