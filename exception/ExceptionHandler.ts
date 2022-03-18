90a2cd139935e063ffcebc76e9ca4e851485f9a1
Felix J <felix.hannes.fj@gmail.com>
2022-03-19 00:23:30 +0100

    linting stuff

--- exception/ExceptionHandler.ts
+++ exception/ExceptionHandler.ts
@@ -1,31 +1,32 @@
 import HttpStatusCode from '../models/HttpStatusCodes';
-import {NetworkError} from "./errors/NetworkError";
-import {AppError} from "./errors/AppError";
+import {NetworkError} from './errors/NetworkError';
+import {AppError} from './errors/AppError';
 
 export class ExceptionHandler {
 
     static handleException(err: any, res: any, next: any) {
         switch (err.constructor) {
             case NetworkError : {
-                ExceptionHandler.handleNetworkError(err, res)
-                break
+                ExceptionHandler.handleNetworkError(err, res);
+                break;
             }
             case AppError: {
-                ExceptionHandler.handleAppError(err, res)
-                break
+                ExceptionHandler.handleAppError(err, res);
+                break;
             }
             default : {
-                res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(`${err.name}: ${err.message}`)
+                res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(`${err.name}: ${err.message}`);
             }
         }
-        next()
+        console.trace(err);
+        next();
     }
 
     private static handleAppError(err: AppError, res: any) {
-			res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(`${err.name}: ${err.message}`)
+			res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(`${err.name}: ${err.message}`);
     }
 
     private static handleNetworkError(err: NetworkError, res: any) {
-        res.status(err.httpCode).send(`${err.name}: ${err.message}`)
+        res.status(err.httpCode).send(`${err.name}: ${err.message}`);
         }
     }
